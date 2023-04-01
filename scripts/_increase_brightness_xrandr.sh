d5a737e69ab945f6d4bb3066162ab8a5b741f681
LGXerxes <ditisluca@yahoo.it>
2023-04-01 21:01:58 +0200

    no overbrightness

--- scripts/_increase_brightness_xrandr.sh
+++ scripts/_increase_brightness_xrandr.sh
@@ -6,8 +6,8 @@ STEP=0.1
 CURRENT_BRIGHTNESS=$(xrandr --verbose | grep -i "Brightness" | awk '{print $2}')
 NEW_BRIGHTNESS=$(echo "$CURRENT_BRIGHTNESS + $STEP" | bc)
 
-if (( $(echo "$NEW_BRIGHTNESS > 1.4" | bc -l) )); then
-  NEW_BRIGHTNESS=1.4
+if (( $(echo "$NEW_BRIGHTNESS > 1" | bc -l) )); then
+  NEW_BRIGHTNESS=1
 fi
 
 xrandr --output $DISPLAY_NAME --brightness $NEW_BRIGHTNESS

18ce52845b4598c57e1467bae93f45c7f71b659e
LGXerxes <ditisluca@yahoo.it>
2023-04-04 18:24:39 +0200

    redit audio switcher

--- install-switch-audio-port.sh
+++ install-switch-audio-port.sh
@@ -4,19 +4,28 @@
 read -r -d '' SWITCH_AUDIO_PORT_SCRIPT <<'EOF'
 #!/bin/bash
 
-SINK_SPEAKERS="alsa_output.pci-0000_09_00.4.analog-stereo"
-SINK_HEADPHONES="alsa_output.usb-Kingston_HyperX_7.1_Audio_00000000-00.analog-stereo"
+SINK_SPEAKERS="alsa_output.pci-0000_09_00.4.3.analog-stereo"
+SINK_HEADPHONES="alsa_output.usb-Kingston_HyperX_7.1_Audio_00000000-00.3.analog-stereo"
 PORT_SPEAKERS="analog-output-lineout"
-PORT_HEADPHONES="analog-output-headphones"
+PORT_HEADPHONES="analog-output"
 
 current_sink=$(pactl list short sinks | grep RUNNING | awk '{print $2}')
+
 if [ "$current_sink" == "$SINK_SPEAKERS" ]; then
+    new_sink="$SINK_HEADPHONES"
     pactl set-sink-port "$SINK_HEADPHONES" "$PORT_HEADPHONES"
-    pactl set-default-sink "$SINK_HEADPHONES"
 else
+    new_sink="$SINK_SPEAKERS"
     pactl set-sink-port "$SINK_SPEAKERS" "$PORT_SPEAKERS"
-    pactl set-default-sink "$SINK_SPEAKERS"
 fi
+
+# Set new default sink and move all audio streams to the new sink
+pactl set-default-sink "$new_sink"
+pactl list short sink-inputs | while read -r stream; do
+    stream_id=$(echo "$stream" | awk '{print $1}')
+    pactl move-sink-input "$stream_id" "$new_sink"
+done
+
 EOF
 
 # Create the switch-audio-port file in /usr/local/bin

66dcae8f4ed8009153acd0f923a4e884533109f7
LGXerxes <ditisluca@yahoo.it>
2023-04-10 10:44:10 +0200

    new switch audio port

--- install-switch-audio-port.sh
+++ install-switch-audio-port.sh
@@ -4,10 +4,26 @@
 read -r -d '' SWITCH_AUDIO_PORT_SCRIPT <<'EOF'
 #!/bin/bash
 
-SINK_SPEAKERS="alsa_output.pci-0000_09_00.4.3.analog-stereo"
-SINK_HEADPHONES="alsa_output.usb-Kingston_HyperX_7.1_Audio_00000000-00.3.analog-stereo"
+#!/bin/bash
+
+# Update these descriptions to match your device names
+SPEAKER_DESC="Starship/Matisse HD Audio Controller Analog Stereo"
+HEADPHONE_DESC="HyperX 7.1 Audio Analog Stereo"
+
+# Get sinks dynamically
+SINK_SPEAKERS=$(pactl list sinks | grep -B 4 -i "$SPEAKER_DESC" | grep 'Name:' | awk '{print $2}')
+SINK_HEADPHONES=$(pactl list sinks | grep -B 4 -i "$HEADPHONE_DESC" | grep 'Name:' | awk '{print $2}')
+
+# Get active ports dynamically
 PORT_SPEAKERS="analog-output-lineout"
-PORT_HEADPHONES="analog-output"
+PORT_HEADPHONES="analog-output-headphones"
+
+
+# Print debugging information
+echo "SINK_SPEAKERS: $SINK_SPEAKERS"
+echo "SINK_HEADPHONES: $SINK_HEADPHONES"
+echo "PORT_SPEAKERS: $PORT_SPEAKERS"
+echo "PORT_HEADPHONES: $PORT_HEADPHONES"
 
 current_sink=$(pactl list short sinks | grep RUNNING | awk '{print $2}')
 
@@ -25,7 +41,6 @@ pactl list short sink-inputs | while read -r stream; do
     stream_id=$(echo "$stream" | awk '{print $1}')
     pactl move-sink-input "$stream_id" "$new_sink"
 done
-
 EOF
 
 # Create the switch-audio-port file in /usr/local/bin

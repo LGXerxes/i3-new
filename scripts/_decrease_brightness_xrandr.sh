9586d4b30bbe5e022788ddf7b840243240da03cc
LGXerxes <ditisluca@yahoo.it>
2023-04-12 11:16:26 +0200

    makes brightness controll work for multi display

--- scripts/_decrease_brightness_xrandr.sh
+++ scripts/_decrease_brightness_xrandr.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-DISPLAY_NAME="DisplayPort-2" # Replace with your display name
 STEP=0.1
 
-CURRENT_BRIGHTNESS=$(xrandr --verbose | grep -i "Brightness" | awk '{print $2}')
-NEW_BRIGHTNESS=$(echo "$CURRENT_BRIGHTNESS - $STEP" | bc)
+for DISPLAY_NAME in $(xrandr | grep " connected" | awk '{print $1}'); do
+  CURRENT_BRIGHTNESS=$(xrandr --verbose | grep -i "${DISPLAY_NAME} connected" -A5 | grep -i "Brightness" | awk '{print $2}')
+  NEW_BRIGHTNESS=$(echo "$CURRENT_BRIGHTNESS - $STEP" | bc)
 
-if (( $(echo "$NEW_BRIGHTNESS < 0" | bc -l) )); then
-  NEW_BRIGHTNESS=0
-fi
+  if (( $(echo "$NEW_BRIGHTNESS <0 " | bc -l) )); then
+    NEW_BRIGHTNESS=0
+  fi
 
-xrandr --output $DISPLAY_NAME --brightness $NEW_BRIGHTNESS
+  xrandr --output $DISPLAY_NAME --brightness $NEW_BRIGHTNESS
+done
